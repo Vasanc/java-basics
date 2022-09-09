9dd7f272c3663233c4b6c0bb0bc1732f4a2c3402
Vasanc <vasanaids@smvec.ac.in>
2022-09-09 15:48:38 +0530

    oops

--- src/Abstraction.java
+++ src/Abstraction.java
@@ -1,7 +1,46 @@
+interface aa {
+    public void display();
+
+    public void show();
+
+}
+
+interface bb {
+    public void dois();
+}
+
+abstract class awe {
+    abstract void baa();
+}
+
+class awes extends awe{
+    void baa(){
+        System.out.println("abc");
+    }
+}
+class Inter implements aa, bb {
+    public void display() {
+        System.out.println("method in aa interface");
+    }
+
+    public void dois() {
+        System.out.println("method in bb interface");
+    }
+
+}
 
 public class Abstraction {
     public static void main(String[] args) {
-
+        Inter a = new Inter() {
+            public void show() {
+                System.out.println("hi all");
+            }
+        };
+        a.display();
+        a.dois();
+        a.show();
+        awes b = new awes();
+        b.baa();
     }
 
 }
